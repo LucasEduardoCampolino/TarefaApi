fe84bec5da9d2653742bd64991590a36df391353
lagartaSadia <campolino.l@gmail.com>
2025-08-30 15:18:15 -0300

    first commit

---
+++ tarefapi/src/main/java/com/example/tarefapi/TarefapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tarefapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefapiApplication.class, args);
+	}
+
+}
---
+++ tarefapi/src/test/java/com/example/tarefapi/TarefapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tarefapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
